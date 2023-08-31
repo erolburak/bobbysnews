5e65f9a5465d10cfc8fb9ca243d5f46f0d38c5c8
Burak Erol <140210017+erolburak@users.noreply.github.com>
2023-08-31 15:51:22 +0200

    initial commit (#1)

---
+++ BobbysNews/BobbysNewsTests/BobbysNewsTests.swift
@@ -0,0 +1,15 @@
+//
+//  BobbysNewsTests.swift
+//  BobbysNewsTests
+//
+//  Created by Burak Erol on 31.08.23.
+//
+
+import XCTest
+
+class BobbysNewsTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+}
---
+++ BobbysNews/BobbysNewsUITests/BobbysNewsUITests.swift
@@ -0,0 +1,24 @@
+//
+//  BobbysNewsUITests.swift
+//  BobbysNewsUITests
+//
+//  Created by Burak Erol on 31.08.23.
+//
+
+import XCTest
+
+final class BobbysNewsUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
