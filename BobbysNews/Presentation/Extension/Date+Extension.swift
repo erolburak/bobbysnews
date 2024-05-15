a0c7a9fa34c3d18936f9d0757ca229239394a2b4
erolburak <erolburak@icloud.com>
2024-05-16 00:05:44 +0200

    add privacy info update iPad version for tests update date formatter

--- BobbysNews/Presentation/Extension/Date+Extension.swift
+++ BobbysNews/Presentation/Extension/Date+Extension.swift
@@ -9,15 +9,22 @@ import Foundation
 
 extension Date {
 
-	// MARK: - Properties
+	// MARK: - Private Properties
 
-	/// Formats date to string in a relative form -> `Yesterday`, `Today`, `Tomorrow`...
-	var toRelative: String {
+	private static let relativeDateFormatter = {
 		let dateFormatter = DateFormatter()
+		dateFormatter.calendar = .current
 		dateFormatter.timeZone = .current
 		dateFormatter.dateStyle = .medium
 		dateFormatter.timeStyle = .short
 		dateFormatter.doesRelativeDateFormatting = true
-		return dateFormatter.string(from: self)
+		return dateFormatter
+	}()
+
+	// MARK: - Properties
+
+	/// Formats date to string in a relative form -> `Yesterday`, `Today`, `Tomorrow`...
+	var toRelative: String {
+		Date.relativeDateFormatter.string(from: self)
 	}
 }
