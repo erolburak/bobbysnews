95a51d7592971561bbc441a93ae411b5ec98c28f
Burak Erol <140210017+erolburak@users.noreply.github.com>
2024-11-07 00:10:02 +0100

    Develop (#121)
    * Update tests
    
    * Update tests
    
    * Update tests

--- BobbysNewsUITests/Presentation/DetailViewTests.swift
+++ BobbysNewsUITests/Presentation/DetailViewTests.swift
@@ -19,20 +19,8 @@ final class DetailViewTests: XCTestCase {
         /// Launch app
         let app = XCUIApplication().appLaunch()
         app.openDetailView(with: app)
-        openCloseShareView(with: app)
         openCloseWebView(with: app)
-    }
-
-    @MainActor
-    private func openCloseShareView(with app: XCUIApplication) {
-        /// Open share view
-        let shareLink = app.buttons["ShareLink"]
-        XCTAssertTrue(shareLink.waitForExistence(timeout: 5))
-        shareLink.tap()
-        /// Close share view
-        let closeButton = app.navigationBars["UIActivityContentView"].buttons["Schließen"]
-        XCTAssertTrue(closeButton.waitForExistence(timeout: 5))
-        closeButton.tap()
+        openShareView(with: app)
     }
 
     @MainActor
@@ -45,5 +33,13 @@ final class DetailViewTests: XCTestCase {
         let closeButton = app.buttons["CloseButton"]
         XCTAssertTrue(closeButton.waitForExistence(timeout: 5))
         closeButton.tap()
+    }
+
+    @MainActor
+    private func openShareView(with app: XCUIApplication) {
+        /// Open share view
+        let shareLink = app.buttons["ShareLink"]
+        XCTAssertTrue(shareLink.waitForExistence(timeout: 5))
+        shareLink.tap()
     }
 }
